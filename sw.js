3e2ba57a0d4589468415caf437d69c814b80c7d0
HPL1130 <aircon.tw@gmail.com>
2025-11-21 16:31:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
+const CACHE_NAME = 'cst-spshop-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/shop_data.json',
+  // 請記得加入您在 images/ 內的所有圖片檔案路徑
+  // '/images/hero.png',
+  // '/images/categories/medical.png', ...
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache match - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
